d6706f45fe45b939e31e7064bc1d3f212ea7e0f2
nihaadk <nihadkeric@gmail.com>
2023-01-15 22:29:02 +0100

    create demo app with dark mode

---
+++ components/ThemeChangeButton.tsx
@@ -0,0 +1,38 @@
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
+import { ThemeMode } from '../enums/ThemeMode.enum';
+
+export default function ThemeChangeButton() {
+  const { systemTheme, setTheme, theme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const currentTheme = theme === ThemeMode.SYSTEM ? systemTheme : theme;
+
+  if (currentTheme === ThemeMode.DARK) {
+    return (
+      <MdLightMode
+        className='w-8 h-8 text-white'
+        role='button'
+        title='Switch to Light'
+        onClick={() => setTheme(ThemeMode.LIGHT)}
+      />
+    );
+  } else {
+    return (
+      <MdDarkMode
+        className='w-8 h-8 text-gray-900'
+        role='button'
+        title='Switch to Dark'
+        onClick={() => setTheme(ThemeMode.DARK)}
+      />
+    );
+  }
+}
---
+++ sections/Hero.tsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+  return (
+    <section className='text-gray-600 body-font'>
+      <div className='container mx-auto flex px-5 py-24 items-center justify-center flex-col'>
+        <div className='text-center lg:w-2/3 w-full'>
+          <h1 className='title-font sm:text-6xl sm:font-bold text-3xl mb-4 font-medium text-indigo-500'>
+            Microdosing synth tattooed vexillologist
+          </h1>
+          <p className='mb-8 leading-relaxed'>
+            Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing
+            tousled. Chambray dreamcatcher trust fund, kitsch vice godard
+            disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh.
+            Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo
+            booth af fingerstache pitchfork.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
