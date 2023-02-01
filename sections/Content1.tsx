9d7a6acc295778ec8b53efa1dae1fbdd442b18c7
nihaadk <nihadkeric@gmail.com>
2023-02-01 23:30:34 +0100

    fixed bug

--- sections/Content1.tsx
+++ sections/Content1.tsx
@@ -53,7 +53,7 @@ export default function Content1() {
           </div>
           
         </div>
-        <UiButton></UiButton>
+        <UiButton color="rose">Read Me!</UiButton>
         {/* <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
           Button
         </button> */}
